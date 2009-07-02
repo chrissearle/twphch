09a76f03e52a3ddb1e38ee30e06ce6ee031f0e1a
Chris Searle <chris@chrissearle.org>
2009-07-02 13:33:33 +0200

    Add interceptor to grab flickr url

---
+++ src/main/java/net/chrissearle/flickrvote/web/FlickrAuthInterceptor.java
@@ -0,0 +1,27 @@
+package net.chrissearle.flickrvote.web;
+
+import com.opensymphony.xwork2.interceptor.Interceptor;
+import com.opensymphony.xwork2.ActionInvocation;
+import org.springframework.beans.factory.annotation.Autowired;
+import net.chrissearle.flickrvote.service.FlickrService;
+
+import java.util.Map;
+
+public class FlickrAuthInterceptor implements Interceptor {
+    @Autowired
+    private FlickrService flickrService;
+
+    public void destroy() {
+    }
+
+    public void init() {
+    }
+
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        Map session = actionInvocation.getInvocationContext().getSession();
+
+        session.put(FlickrVoteWebConstants.FLICKR_LOGIN_URL, flickrService.getLoginUrl().toExternalForm());
+
+        return actionInvocation.invoke();
+    }
+}
---
+++ src/main/java/net/chrissearle/flickrvote/web/FlickrVoteWebConstants.java
@@ -0,0 +1,5 @@
+package net.chrissearle.flickrvote.web;
+
+public interface FlickrVoteWebConstants {
+    String FLICKR_LOGIN_URL = "flickrLoginUrl";
+}
