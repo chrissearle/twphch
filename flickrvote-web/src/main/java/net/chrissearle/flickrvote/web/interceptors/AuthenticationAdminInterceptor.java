c8a1aa4e0e41a09301f710cd95aa4423f9a1968b
Chris Searle <chris@chrissearle.org>
2009-07-16 20:25:44 +0200

    Rename - specific to admin

---
+++ flickrvote-web/src/main/java/net/chrissearle/flickrvote/web/interceptors/AuthenticationAdminInterceptor.java
@@ -0,0 +1,31 @@
+package net.chrissearle.flickrvote.web.interceptors;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+import net.chrissearle.flickrvote.service.model.PhotographerInfo;
+
+import java.util.Map;
+
+public class AuthenticationAdminInterceptor implements Interceptor {
+    public void destroy() {
+    }
+
+    public void init() {
+    }
+
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        Map<String, Object> session = actionInvocation.getInvocationContext().getSession();
+
+        PhotographerInfo photographer = (PhotographerInfo) session.get("flickrUser");
+
+        if (photographer == null) {
+            return "notloggedin";
+        }
+
+        if (!photographer.isAdministrator()) {
+            return "notadmin";
+        }
+
+        return actionInvocation.invoke();
+    }
+}
