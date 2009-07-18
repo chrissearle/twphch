9c58467015a46a4173b49e631761ca4c8f927017
Chris Searle <chris@chrissearle.org>
2009-07-18 21:31:45 +0200

    Remove test - we don't need it and we're not going to use the twitter timeline any time soon either.

--- flickrvote-twitter/src/main/java/net/chrissearle/flickrvote/twitter/Twitter4jService.java
+++ flickrvote-twitter/src/main/java/net/chrissearle/flickrvote/twitter/Twitter4jService.java
@@ -3,11 +3,7 @@ package net.chrissearle.flickrvote.twitter;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import twitter4j.Status;
 import twitter4j.Twitter;
-import twitter4j.TwitterException;
-
-import java.util.List;
 
 @Service
 public class Twitter4jService implements TwitterService {
@@ -20,17 +16,7 @@ public class Twitter4jService implements TwitterService {
         if (logger.isInfoEnabled()) {
             logger.info("Tweeting: " + text);
         }
-    }
 
-    public List<Status> getTimeline() {
-        try {
-            return twitter.getFriendsTimeline();
-        } catch (TwitterException e) {
-            if (logger.isInfoEnabled()) {
-                logger.info("Failed to get twitter timeline", e);
-            }
-        }
-
-        return null;
+        // TODO - Tweet
     }
 }
--- flickrvote-twitter/src/main/java/net/chrissearle/flickrvote/twitter/TwitterService.java
+++ flickrvote-twitter/src/main/java/net/chrissearle/flickrvote/twitter/TwitterService.java
@@ -1,11 +1,5 @@
 package net.chrissearle.flickrvote.twitter;
 
-import twitter4j.Status;
-
-import java.util.List;
-
 public interface TwitterService {
     void twitter(String text);
-
-    List<Status> getTimeline();
 }
