0a81dc248cb06ff6e9f5f50b178a211a69f16d0b
Chris Searle <chris@chrissearle.org>
2009-07-21 20:25:42 +0200

    Consolidate score

--- flickrvote-web/src/main/java/net/chrissearle/flickrvote/web/admin/ScoreAction.java
+++ flickrvote-web/src/main/java/net/chrissearle/flickrvote/web/admin/ScoreAction.java
@@ -1,7 +1,10 @@
 package net.chrissearle.flickrvote.web.admin;
 
 import com.opensymphony.xwork2.ActionSupport;
+import net.chrissearle.flickrvote.service.ChallengeService;
 import net.chrissearle.flickrvote.service.PhotographyService;
+import net.chrissearle.flickrvote.service.model.ChallengeInfo;
+import net.chrissearle.flickrvote.service.model.ImageInfo;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,8 +17,36 @@ public class ScoreAction extends ActionSupport {
 
     private List<Long> score;
 
+    private ChallengeInfo challenge;
+
+    private String tag;
+
+    private List<ImageInfo> images;
+
     @Autowired
     private PhotographyService photographyService;
+
+    @Autowired
+    private ChallengeService challengeService;
+
+    @Override
+    public String input() throws Exception {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Challenge ID " + tag);
+        }
+
+        challenge = challengeService.getChallenge(tag);
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Challenge " + challenge);
+        }
+
+        if (challenge != null) {
+            images = challengeService.getImagesForChallenge(tag);
+        }
+
+        return INPUT;
+    }
 
     @Override
     public String execute() throws Exception {
@@ -38,5 +69,18 @@ public class ScoreAction extends ActionSupport {
 
     public void setScore(List<Long> score) {
         this.score = score;
+    }
+
+
+    public ChallengeInfo getChallenge() {
+        return challenge;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public List<ImageInfo> getImages() {
+        return images;
     }
 }
