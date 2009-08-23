83126e2ba5f9a19943a8acb43ec110571e6d75d5
Chris Searle <chris@chrissearle.org>
2009-08-23 11:11:29 +0200

    Apply Apache 2.0 license

--- flickrvote-service-spring/src/main/java/net/chrissearle/flickrvote/service/Comparators.java
+++ flickrvote-service-spring/src/main/java/net/chrissearle/flickrvote/service/Comparators.java
@@ -1,11 +1,27 @@
+/*
+ * Copyright 2009 Chris Searle
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package net.chrissearle.flickrvote.service;
 
-import net.chrissearle.flickrvote.service.model.ImageItem;
-import net.chrissearle.flickrvote.service.model.ChallengeSummary;
 import net.chrissearle.flickrvote.model.Image;
+import net.chrissearle.flickrvote.service.model.ChallengeSummary;
+import net.chrissearle.flickrvote.service.model.ImageItem;
 
-import java.util.Comparator;
 import java.io.Serializable;
+import java.util.Comparator;
 
 public class Comparators {
     static class ImageItemSortByVoteCount implements Comparator<ImageItem>, Serializable {
